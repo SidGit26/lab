0021ba1ef13fe043933943eb3f116a992b5ebafe
Siddharth <siddzzone123@gmail.com>
2023-09-19 09:31:04 +0530

    first commit

---
+++ src/test/java/com/sidd/AppTest.java
@@ -0,0 +1,20 @@
+package com.sidd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
